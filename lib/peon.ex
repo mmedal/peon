8689a48def77a28a80bd8d8e6bf260e7f35950bd
Matthew Medal <matt.medal@gmail.com>
2017-01-30 14:27:49 -0800

    initial commit

---
+++ lib/peon.ex
@@ -0,0 +1,18 @@
+defmodule Peon do
+  @moduledoc """
+  Documentation for Peon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Peon.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
